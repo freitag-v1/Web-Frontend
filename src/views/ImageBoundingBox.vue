ea26896e62ef1f202dc34c56b67417141ff80075
nahyun <nahyun858@gmail.com>
2020-06-08 12:43:29 +0900

    이미지 바운딩 박스 기능 구현(서버랑 연동해야한다)

--- src/views/ImageBoundingBox.vue
+++ src/views/ImageBoundingBox.vue
@@ -29,18 +29,37 @@
             <img id="exampleImage" :src = "downloadUrl" v-if="downloadUrl != ''" style="width: 400px; height: 300px;"/>
         </div>
         </b-card-text>
-    <div v-for="i in imageCount">
-        <br>
+    <b-card-footer style="font-weight: bolder">이미지 바운딩 박스 작업</b-card-footer>
+    <b-card-text class = "content">
+    <img id="boundingBoxImage" :src = "boundingBoxProblemUrl" v-if="boundingBoxProblemUrl != ''"/>
+    <b-form-file multiple 
+                v-model="imageContent"
+                :state="Boolean(imageContent)"
+                placeholder="Choose a file or drop it here..."
+                drop-placeholder="Drop file here..."
+                hidden @change="onChangeImages"
+                accept="image/*"
+                ></b-form-file>
+        <canvas id="boundingImage" width="800" height="600"></canvas>
+        <br> 
+    </b-card-text>
+    <div class="overflow-auto" style="margin : auto;">
+        <b-pagination id="pagination"
+         v-model="currentPage"
+          :total-rows="problemList.length"
+            per-page= 1
+            first-text="First"
+            prev-text="Prev"
+            next-text="Next"
+            last-text="Last"></b-pagination>
     </div>
     <div class = "buttons">
-        <b-button size="lg" variant="primary" v-on:click="addImage">
-                <b-icon icon="file-earmark-plus" aria-label="Help"></b-icon> Add
+        <b-button size="lg" variant="primary" v-on:click="clearBox">
+            박스 지우기
         </b-button>
         <b-button size="lg" variant="warning" v-on:click="upload" v-model ="createCollection">
                 <b-icon icon="upload"></b-icon> 업로드
         </b-button>
-        <img :src = "downloadUrl" v-if="downloadUrl != ''" style="width: 400px; height: 500px;"/>
-        <AudioUpload v-if="audioUrl != ''" :value="audioUrl"/>
     </div>
     <br>
     </b-card>
@@ -50,11 +69,11 @@
 </template>
 <script>
 import axios from 'axios';
-import VueCropper from 'vue-cropperjs';
-import AudioUpload from '../components/AudioUpload.vue';
+//import VueCropper from 'vue-cropperjs';
 
 var ImageList = new Array();
 var dataState = false; //데이터가 업로드 되었는지의 여부
+
 
 const endpoint = 'kr.object.ncloudstorage.com';
 const region = 'kr-standard';
@@ -89,22 +108,75 @@ s3Client.interceptors.request.use(function (config) {
 
 const local_file_path = '../assets';
 
+//바운딩 박스를 그릴 때 필요한 데이터
+
+var ctx;
+var canvas;
+var canvasX = 0;
+var canvasY = 0;
+var last_mouseX = 0;
+var last_mouseY = 0;
+var mouseX = 0;
+var mouseY = 0;
+var isMouseDown = false;
+
+
+
+
+//마우스로 그림을 그리는 코드
+
+//마우스가 눌러질 때
+function down(e){
+    last_mouseX = e.offsetX - canvasX;
+    last_mouseY = e.offsetY - canvasY;
+    isMouseDown = true;
+}
+
+//눌러진 마우스가 놓일 때
+function up(e) {
+    if(isMouseDown){
+        ctx.beginPath();
+        var width = mouseX - last_mouseX;
+        var height = mouseY - last_mouseY;
+        ctx.rect(last_mouseX, last_mouseY, width, height);
+        ctx.strokeStyle = '#1E3269';
+        ctx.lineWidth = 5;
+        ctx.stroke();
+        //이게 박스 좌표여서 이거를 서버에 보내면 된다. 
+        console.log(last_mouseX, last_mouseY, width, height);
+    }
+    isMouseDown = false;
+}
+
+//마우스가 움직이는 동안
+function move(e){
+    mouseX = e.offsetX - canvasX;
+    mouseY = e.offsetY - canvasY;
+
+}
+
+
  export default {
     name: 'ImageCollection',
     components : {
-        //ImageUpload
-        AudioUpload,
+        
     },
     data() {
         return {
-            project: null,
+            problemList:[],
+            problem: '',
             classNameList: [],
-            imageCount: 1,
+            project: '',
             createCollection: false,
             bucketName: '',
+            imageContent: '',
+            imageUrl:'',
+            ctx: '',
+            canvas: '',
+            bounding: '',
+            boundingBoxProblemUrl : '',
+            currentPage: 1,
             downloadUrl: '',
-            audioUrl: '',
-            
 
         }
     },
@@ -113,14 +185,28 @@ const local_file_path = '../assets';
     },
     async created() {
         this.fetchData();
-        this.examplaDataDownload();
+        //this.examplaDataDownload();
         var searchproject = await localStorage.getItem('searchProject');//this.$route.params.project;
         //console.log(JSON.parse(searchproject));
         this.bucketName = JSON.parse(searchproject).projectDto.bucketName;
+        //canvas를 생성해서 여기 위에서 진행을 할 수 있도록 
+        this.canvas = document.getElementById("boundingImage");
+        //var boundingImageData = '';
+        this.ctx = this.canvas.getContext('2d');
+        this.bounding = this.canvas.getBoundingClientRect(); //뭔가 바운딩을 하면 그림을 그려주는 느낌
+        // 캔버스의 마우스 이벤트 핸들러 등록
+        ctx = this.ctx;
+        canvas = this.canvas;
+        this.canvas.addEventListener( "mousemove", move);
+        this.canvas.addEventListener( "mousedown", down);
+        this.canvas.addEventListener( "mouseup", up);
+
+        //this.ctx.strokeRect(0,0,800,600);
 
     },
     watch : {
-        '$route' : 'fetchData'
+        '$route' : 'fetchData',
+
     },
     beforeMount() {
             window.addEventListener("beforeunload", this.preventNav); //웹페이지 닫을 때 일어나는거 
@@ -146,40 +232,41 @@ const local_file_path = '../assets';
             this.classNameList = JSON.parse(searchproject).classNameList;//this.$route.params.classList;
             console.log(this.project);
         },
-        async examplaDataDownload(){
-            var exampleData = await localStorage.getItem('exampleContent');
-            console.log(exampleData);
-            if(exampleData == null){
-                    s3Client.get("/"+this.project.bucketName+"/"+this.project.exampleContent, {
-                        responseType : 'blob',
-                    }).then((res) =>{
-                        var exampleFile = new File([res.data], this.project.exampleContent,{ type: res.headers['content-type'], lastModified : Date.now() } );
-                        const url = URL.createObjectURL(new Blob([res.data], { type: res.headers['content-type'] }));
-                        console.log(url);
-                            this.downloadUrl = url;
-                                var content = {
-                                    type : res.data.type,
-                                    file : exampleFile,
-                                }
-                            localStorage.exampleContent = JSON.stringify(content);
-                    });
-            }
-            else {
-                var exampleLocal = await localStorage.getItem('exampleContent');
-                var exampleLocalDataType = JSON.parse(exampleLocal).type;
-                var exampleLocalData = JSON.parse(exampleLocal).file;
+        // async examplaDataDownload(){
+        //     var exampleData = await localStorage.getItem('exampleContent');
+        //     console.log(exampleData);
+        //     if(exampleData == null){
+        //             s3Client.get("/"+this.project.bucketName+"/"+this.project.exampleContent, {
+        //                 responseType : 'blob',
+        //             }).then((res) =>{
+        //                 var exampleFile = new File([res.data], this.project.exampleContent,{ type: res.headers['content-type'], lastModified : Date.now() } );
+        //                 const url = URL.createObjectURL(new Blob([res.data], { type: res.headers['content-type'] }));
+        //                 console.log(url);
+        //                     this.downloadUrl = url;
+        //                         var content = {
+        //                             type : res.data.type,
+        //                             file : exampleFile,
+        //                         }
+        //                     localStorage.exampleContent = JSON.stringify(content);
+        //             });
+        //     }
+        //     else {
+        //         var exampleLocal = await localStorage.getItem('exampleContent');
+        //         var exampleLocalDataType = JSON.parse(exampleLocal).type;
+        //         var exampleLocalData = JSON.parse(exampleLocal).file;
 
-                this.downloadUrl = URL.createObjectURL(exampleLocalData);
-            }   
-        },
+        //         this.downloadUrl = URL.createObjectURL(exampleLocalData);
+        //     }   
+        // },
         preventNav(event) {
                 if (!dataState || this.createCollection) return;
                 event.preventDefault();
                 // Chrome requires returnValue to be set.
                 event.returnValue = "";
         },
-        addImage() {
-            this.imageCount += 1;
+        clearBox() {
+            this.ctx = clearRect(0, 0, canvas.width, canvas.height);
+
         },
         upload() { //formData 리스트!
             //var imageFormData = new Array(); 뭔가 formdata 리스트가 아니라 formdata에 append해서 보내는거 같다
@@ -193,7 +280,25 @@ const local_file_path = '../assets';
            console.log("=========================================");
            
         },
-
+        onChangeImages(e) {
+            console.log(e.target.files);
+            const file = e.target.files[0];
+            this.imageUrl = URL.createObjectURL(file);    
+            //this.$refs.cropper.replace(this.imageUrl);
+            var originalImage = new Image();
+            originalImage.src = this.imageUrl;
+            //boundingImageData = this.imageUrl;
+            var imageContext = this.ctx;
+            
+            originalImage.onload = function() {
+                var scale = Math.min(canvas.width / originalImage.width, canvas.height / originalImage.height);
+                var x = (canvas.width / 2) - (originalImage.width / 2) * scale;
+                var y = (canvas.height / 2) - (originalImage.height / 2) * scale;
+                imageContext.drawImage(originalImage, x, y, originalImage.width * scale, originalImage.height * scale);
+            }
+            
+        },
+        
     }
 
  }
@@ -226,6 +331,8 @@ const local_file_path = '../assets';
     margin: 5px;
     width: 150px;
 }
-
+canvas {
+    cursor : crosshair;
+}
 </style>
 
